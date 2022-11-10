50ef227dbcaeb6ea3fb9a031b8ea396f74aa7262
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2022-11-11 02:50:32 +0900

    feat():add 폰번호,이멜 중복체크, 회원정보업뎃메소드 서비스 추가

--- semiProject/src/member/model/MemberService.java
+++ semiProject/src/member/model/MemberService.java
@@ -13,71 +13,110 @@ public class MemberService {
 	
 	public Member loginMember(String memId, String memPw) {
 		Connection conn = JDBCTemplate.getConnection();
-	
 		Member m = new MemberDao().loginMember(conn,memId,memPw);
 		
 		JDBCTemplate.close(conn);
-		
 		return m;
 	}
 
 	public int insertMember(Member m) {
-		
 		Connection conn = JDBCTemplate.getConnection();
-		
 		int result = new MemberDao().insertMember(conn,m);
 		
 		if(result>0) {
 			JDBCTemplate.commit(conn);
-		}
-		else {
+		}else {
 			JDBCTemplate.rollback(conn);
 		}
-		
 		JDBCTemplate.close(conn);
-		
 		return result;
 	}
 	
 	public int idDuplicationCheck(String memId) {
-		
 		Connection conn = JDBCTemplate.getConnection();
-	
 		int result = new MemberDao().idDuplicationCheck(conn,memId);
 		
 		JDBCTemplate.close(conn);
+		return result;
+	}
+	
+	public int phoneDuplicationCheck(String memPhone) {
+		Connection conn = JDBCTemplate.getConnection();
+		int result = new MemberDao().phoneDuplicationCheck(conn,memPhone);
 		
+		JDBCTemplate.close(conn);
+		return result;
+	}
+	
+	public int emailDuplicationCheck(String memEmail) {
+		Connection conn = JDBCTemplate.getConnection();
+		int result = new MemberDao().emailDuplicationCheck(conn,memEmail);
+		
+		JDBCTemplate.close(conn);
 		return result;
 	}
 
 	public MemberId findId(String memName, String memPhone) {
 		Connection conn = JDBCTemplate.getConnection();
-		
 		MemberId m = new MemberDao().findId(conn,memName,memPhone);
 		
 		JDBCTemplate.close(conn);
-		
 		return m;
 	}
 
 	public MemberPw findPw(String memId, String memName, String memPhone) {
 		Connection conn = JDBCTemplate.getConnection();
-		
 		MemberPw mpw = new MemberDao().findPw(conn,memId,memName,memPhone);
 		
 		JDBCTemplate.close(conn);
-		
 		return mpw;
 	}
 
 	public Order loginNoMember(String reciverName, String orderNo) {
 		Connection conn = JDBCTemplate.getConnection();
-		
 		Order o = new MemberDao().loginNoMember(conn,reciverName,orderNo);
 		
 		JDBCTemplate.close(conn);
-		
 		return o;
 	}
+
+	public Member updateMember(Member loginMem) {
+		Connection conn = JDBCTemplate.getConnection();
+		int result = new MemberDao().updateMember(conn,loginMem);
+		Member updateMem = null;
+		
+		if(result>0) {
+			JDBCTemplate.commit(conn);
+			//update는 성공했으니 다시 dao로 가서 조회
+			updateMem = new MemberDao().selectMember(conn,loginMem.getMemId());
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		JDBCTemplate.close(conn);
+		return updateMem;
+	}
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
