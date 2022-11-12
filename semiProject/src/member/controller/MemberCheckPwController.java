e7b4c8322d3dc85657e5d213e203134c2079a9a3
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2022-11-12 13:16:39 +0900

    feat(): 비번 체크로 가는 컨트롤러 추가

---
+++ semiProject/src/member/controller/MemberCheckPwController.java
@@ -0,0 +1,40 @@
+package com.semi.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberCheckPwController
+ */
+@WebServlet("/pwcheckform.me")
+public class MemberCheckPwController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemberCheckPwController() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("views/member/memberCheckPwForm.jsp").forward(request, response);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
