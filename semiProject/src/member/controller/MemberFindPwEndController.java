f0a7ed00ce3b9d5d0f90e4583529b388410fdf53
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2022-11-12 13:12:14 +0900

    feat(): 비번 찾기 끝 컨트롤러 추가

--- semiProject/src/member/controller/MemberFindPwEndController.java
+++ semiProject/src/member/controller/MemberFindPwEndController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.semi.member.model.service.MemberService;
+import com.semi.member.model.vo.Member;
 import com.semi.member.model.vo.MemberId;
 import com.semi.member.model.vo.MemberPw;
 
@@ -25,24 +26,38 @@ public class MemberFindPwEndController extends HttpServlet {
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
 		
 		String memId = request.getParameter("memId");
-		String memName = request.getParameter("memName");
-		String memPhone = request.getParameter("memPhone");
-		MemberPw mpw = new MemberService().findPw(memId,memName, memPhone);
-	
-		try {
-			if(!mpw.equals(null)) {				
-				System.out.println("비밀번호 조회 성공");
-				request.setAttribute("mpw", mpw);
-				request.getRequestDispatcher("views/member/memberFindPwEnd.jsp").forward(request, response);
-			}
-		} catch(NullPointerException e){
-			e.printStackTrace();
-			HttpSession session = request.getSession();
-			session.setAttribute("alertMsg", "없는 회원입니다.");
+		String memPw = request.getParameter("memPw");
+		Member memIdPw = new Member(memId,memPw);
+		
+		int modifyPwResult = new MemberService().modifyPw(memIdPw);
+		
+		if(modifyPwResult > 0) {
+			request.getRequestDispatcher("views/member/memberFindPwEnd.jsp").forward(request, response);
+		}else {
+			session.setAttribute("alertMsg", "수정에 실패했습니다.");
 			response.sendRedirect(request.getContextPath());
 		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
